bae621ba2f32ecb9a79789462bac3ec72c0fe11b
Frogluba <146203655+Frogluba@users.noreply.github.com>
2024-01-10 20:05:18 +0200

    Map

--- Assets/Ghost.cs
+++ Assets/Ghost.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 0.7f;
+    public float duration = 3f; // laikas po kurio veikejas prades judeti i kita puse
 
-    // Update is called once per frame
+    private float timer = 0f;
+    private bool moveRight = true;
+
     void Update()
     {
+        timer += Time.deltaTime;
+
+        // Objekto judejimas
+        if (moveRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
         
+        if (timer >= duration)
+        {
+            moveRight = !moveRight;
+            timer = 0f; //resetiname timeri
+        }
     }
 }
