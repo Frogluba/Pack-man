91447b3a05deed1989dd95f0ece7ba1d0aa65bf7
Frogluba <146203655+Frogluba@users.noreply.github.com>
2024-01-17 19:57:04 +0200

    pragaras

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Packman movement;
+
+    private void Awake()
+    {
+        this.movement = GetComponent<Packman>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movement.SetDirection(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movement.SetDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movement.SetDirection(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movement.SetDirection(Vector2.right);
+        }
+    }
+}
